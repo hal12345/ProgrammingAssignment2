24e0f05e16b582c6de4da250ef38a224870d207b
hal12345 <hal12345@users.noreply.github.com>
2017-01-15 09:10:08 +0100

    my assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix takes a matrix as input an create a list of function
+## to interact with the object
+## this function stores 2 matrices: x=original matrix, i=inverse of x
+## set stores the "original" matrix
+## get retrieve the "original" matrix
+## setinverse stores the inverse matrix
+## getinverse retrieve the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## x must be an object of "makeCacheMatrix class" eventhough it is eventually an R list
+## if $getinverse() is NULL it means that the matrix has never been "solved"
+## hence solved in i variable and stored with setinverse for future use
+## else
+## just printing a message for this sheer assignment otherwise useless
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
